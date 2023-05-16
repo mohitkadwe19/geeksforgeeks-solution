4679ed140792de9f0d8637cf8c93ca12061368e3
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-05-16 22:32:14 +0530

    📦 NEW: Cutting Binary String

---
+++ Cutting-Binary-String.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  bool isPowerOfFive(long long N)
+  {
+
+    while (N > 1)
+    {
+      if (N % 5 != 0)
+        return 0;
+      N /= 5;
+    }
+    return 1;
+  }
+
+  int helper(int i, string &s, long long sum)
+  {
+
+    if (i >= s.size())
+    {
+      if (sum == 0) // if the string is divided successfully
+        return 0;
+      return INT_MAX;
+    }
+
+    sum = sum * 2 + (s[i] - '0');
+    if (sum == 0) // if current cut is starting have leading zeros
+      return INT_MAX;
+
+    int op1 = INT_MAX, op2 = INT_MAX;
+    if (isPowerOfFive(sum))
+    {
+      int temp = helper(i + 1, s, 0);
+      if (temp != INT_MAX)
+        op1 = 1 + temp;
+    }
+    op2 = helper(i + 1, s, sum);
+
+    return min(op1, op2);
+  }
+
+  int cuts(string s)
+  {
+    int ans = helper(0, s, 0);
+    return ans == INT_MAX ? -1 : ans;
+  }
+};
+
+int main()
+{
+  string s = "101101101";
+  Solution obj;
+  cout << obj.cuts(s) << endl;
+  return 0;
+}
