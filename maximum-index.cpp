d33a77a6b780f68ede19beeec489a7b0cca3f519
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-07-03 23:20:28 +0530

    📦 NEW: maximum-index

---
+++ maximum-index.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int maxIndexDiff(int arr[], int n)
+  {
+    int maxDiff;
+    int i, j;
+
+    int *LMin = new int[(sizeof(int) * n)];
+    int *RMax = new int[(sizeof(int) * n)];
+
+    LMin[0] = arr[0];
+    for (i = 1; i < n; ++i)
+      LMin[i] = min(arr[i], LMin[i - 1]);
+
+    RMax[n - 1] = arr[n - 1];
+    for (j = n - 2; j >= 0; --j)
+      RMax[j] = max(arr[j], RMax[j + 1]);
+
+    i = 0, j = 0, maxDiff = -1;
+    while (j < n && i < n)
+    {
+      if (LMin[i] <= RMax[j])
+      {
+        maxDiff = max(maxDiff, j - i);
+        j++;
+      }
+      else
+        i++;
+    }
+
+    return maxDiff;
+  }
+};
+
+int main()
+{
+  int n = 2;
+  int arr[n] = {1, 2};
+  Solution obj;
+  cout << obj.maxIndexDiff(arr, n) << endl;
+  return 0;
+}
