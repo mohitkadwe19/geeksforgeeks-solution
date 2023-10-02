9454ef3896edd5d1862632ecd7f27f92f634907e
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-10-02 23:07:01 +0530

    📦 NEW: number-of-distinct-subsequences

---
+++ number-of-distinct-subsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    int distinctSubsequences(String s) {
+        int mod = 1000000007;
+        int dp[] = new int[s.length()+1];
+        dp[0] = 1;
+        
+        int ch[] = new int[26];
+        
+        for(int i=1;i<dp.length;i++){
+            int val = ch[s.charAt(i-1)-'a'];
+            dp[i] = ((2 * dp[i-1])%mod) - val;
+            dp[i] %= mod;
+            ch[s.charAt(i-1) - 'a'] = dp[i-1];
+        }
+        int ans = (dp[s.length()]+mod)%mod;
+        return ans;
+    }
+    public static void main(String[] args){
+        String S = "abc";
+        Solution s = new Solution();
+        System.out.println(s.distinctSubsequences(S));
+    }
+}
