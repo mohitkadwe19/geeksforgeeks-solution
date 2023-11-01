4741c2f340d53060800a0ddbb1cb430fa8d1e20a
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-11-02 00:37:02 +0530

    📦 NEW: frequency-of-array-elements

---
+++ frequency-of-array-elements.java
@@ -0,0 +1,16 @@
+class Solution{
+    //Function to count the frequency of all elements from 1 to N in the array.
+    public static void frequencyCount(int arr[], int N, int P)
+    {
+         HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<N;i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+        for(int i=0;i<N;i++){
+            if(map.get(i+1) != null)
+                arr[i] = map.get(i+1);
+            else
+                arr[i] = 0;
+        }
+    }
+}
