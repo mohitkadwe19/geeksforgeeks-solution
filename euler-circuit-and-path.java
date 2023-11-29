77308e6c4836c57882d73d6b6c8b87e78b820926
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-11-29 22:40:06 +0530

    📦 NEW: euler-circuit-and-path

---
+++ euler-circuit-and-path.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+  public int isEulerCircuit(int V, List<Integer>[] adj) {
+    int oddCount = 0;
+    for (int i = 0; i < V; i++) {
+      if (adj[i].size() % 2 != 0) {
+        oddCount += 1;
+      }
+    }
+    if (oddCount == 0)
+      return 2;
+    else if (oddCount == 2)
+      return 1;
+    else
+      return 0;
+  }
+}
